b0d90a64d91b30a2a9933ed2d50154207204cbde
TonyCostantini <156263194+TonyCostantini@users.noreply.github.com>
2024-02-24 10:20:22 +0000

    Updated code

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add JavaScript below
+ScrollReveal().reveal('.image1', {
+    delay: 500,
+    distance: '500px',
+    opacity: 0.01,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.image2', {
+    delay: 750,
+    distance: '1000px',
+    opacity: 0.01,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.image3', {
+    delay: 1000,
+    distance: '1500px',
+    opacity: 0.01,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.image4', {
+    delay: 1250,
+    distance: '2000px',
+    opacity: 0.01,
+    easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.image5', {
+    delay: 1500,
+    distance: '2500px',
+    opacity: 0.01,
+    easing: 'ease-in-out',
+});
